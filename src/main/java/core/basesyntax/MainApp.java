04730011cf9aecfdfc9dda902b8bc0aad9d86fb2
Łukasz Kubicki <remote.pc.kubica@gmail.com>
2026-09-17 14:36:11 +0200

    Format machine classes

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,11 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-    Machine [] machine = {new Truck(), new Bulldozer(), new  Excavator()};
-
-    for(int i = 0; i < machine.length; i++){
-        machine[i].doWork();
-        machine[i].stopWork();
-    }
+        Machine[] machine = {new Truck(), new Bulldozer(), new Excavator()};
+        for (int i = 0; i < machine.length; i++) {
+            machine[i].doWork();
+            machine[i].stopWork();
+        }
     }
 }
